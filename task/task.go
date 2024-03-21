e2c448b75621387a1c4aa012135a285ee504e5d1
Granit Krasniqi <krasniqigranit59@gmail.com>
2024-03-21 23:03:55 +0100

    add Task

--- task/task.go
+++ task/task.go
@@ -1,1 +1,33 @@
 package task
+
+import (
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"time"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+type Task struct {
+	ID            uuid.UUID
+	ContainerID   string
+	Name          string
+	State         State
+	Image         string
+	CPU           float64
+	Memory        int64
+	Disk          int64
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
