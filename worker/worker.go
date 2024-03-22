4d184bc1af993887d28f5961202df621e8912db9
Granit Krasniqi <krasniqigranit59@gmail.com>
2024-03-22 20:00:00 +0100

    add Worker

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,14 @@
 package worker
+
+import (
+	"cube/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
