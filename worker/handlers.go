9b38767e5f0f14729921820510a65a397fb70227
Granit Krasniqi <krasniqigranit59@gmail.com>
2024-03-31 21:22:24 +0200

    add StopTaskHandler

--- worker/handlers.go
+++ worker/handlers.go
@@ -4,6 +4,8 @@ import (
 	"cube/task"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"log"
 	"net/http"
 )
@@ -41,4 +43,27 @@ func (a *Api) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(a.Worker.GetTasks())
+}
+
+func (a *Api) StopTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskId := chi.URLParam(r, "taskID")
+	if taskId == "" {
+		log.Printf("No taskID passed in request.")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	tID, _ := uuid.Parse(taskId)
+	_, ok := a.Worker.Db[tID]
+	if !ok {
+		log.Printf("No task with ID %v found", taskId)
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	taskToStop := a.Worker.Db[tID]
+	taskCopy := *taskToStop
+	taskCopy.State = task.Completed
+	a.Worker.AddTask(taskCopy)
+
+	log.Printf("Added task %v to stop container %v\n", taskCopy.ID, taskCopy.ContainerID)
+	w.WriteHeader(http.StatusAccepted)
 }
